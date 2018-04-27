a77d829b976b5023ff3f2df3855e7add3ab52ccd
nguyenhaiduc1994 <38686202+nguyenhaiduc1994@users.noreply.github.com>
2018-04-27 10:37:40 +0700

    Add files via upload

--- GrabberShow.java
+++ GrabberShow.java
@@ -9,6 +9,8 @@ import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 
+import Barcode.BarcodeReader;
+
 import static org.bytedeco.javacpp.opencv_core.IplImage;
 import static org.bytedeco.javacpp.opencv_core.cvFlip;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvSaveImage;
@@ -21,18 +23,20 @@ import javax.imageio.ImageIO;
 
 //import org.bytedeco.javacpp.opencv_core.IplImage;
 
-
-/**
- * Created by gtiwari on 1/3/2017.
- */
-
-public class GrabberShow implements Runnable {
+public class GrabberShow implements Runnable{
     final int INTERVAL = 100;///you may use interval
-    CanvasFrame canvas = new CanvasFrame("Web Cam");
+    CanvasFrame canvas = new CanvasFrame("Barcode Scanner");
+    private String code;
 
     public GrabberShow() {
         canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
     }
+
+    public String getCode() {
+    	return code;
+    }
+    
+    //convert IPL Image to Buffered Image
     public static BufferedImage IplImageToBufferedImage(IplImage src) {
 	    OpenCVFrameConverter.ToIplImage grabberConverter = new OpenCVFrameConverter.ToIplImage();
 	    Java2DFrameConverter paintConverter = new Java2DFrameConverter();
@@ -41,36 +45,25 @@ public class GrabberShow implements Runnable {
 	}
     
     public void run() {
-
         FrameGrabber grabber = new VideoInputFrameGrabber(0); // 1 for next camera
         OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
         IplImage img;
-        int i = 0;
         try {
             grabber.start();
             Result result = null;
             while (result == null) {
                 Frame frame = grabber.grab();
-
                 img = converter.convert(frame);
-
-                //the grabbed frame will be flipped, re-flip to make it right
-                //cvFlip(img, img, 1);// l-r = 90_degrees_steps_anti_clockwise
-
-                //save
-                //cvSaveImage((i++) + "-aa.jpg", img);
-
                 canvas.showImage(converter.convert(img));
-                //add Scanner to test
+                //Scanner
                 BufferedImage imgg = IplImageToBufferedImage(img);
                 BinaryBitmap binaryBitmap;
                 try {
-                    binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(imgg)));
+                	binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(imgg)));
                     result = new MultiFormatReader().decode(binaryBitmap);
                     System.out.println(result.getText());
                 } catch (NotFoundException e) {
-                	//e.printStackTrace();
-                	System.out.println("Failed");
+                	System.out.println("Scanning...");
                 }
                 //scanner's end
                 Thread.sleep(INTERVAL);
@@ -79,7 +72,6 @@ public class GrabberShow implements Runnable {
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) {
         GrabberShow gs = new GrabberShow();
         Thread th = new Thread(gs);
